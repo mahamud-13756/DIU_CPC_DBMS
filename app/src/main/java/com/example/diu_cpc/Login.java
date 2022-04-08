82988c0b1bcc746a9146b603306ca60984973b5d
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-04-08 16:34:53 +0600

    Layout Design

---
+++ app/src/main/java/com/example/diu_cpc/Login.java
@@ -0,0 +1,20 @@
+package com.example.diu_cpc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.diu_cpc.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
